519820d6d88283099edea34f7463512b97c7d304
DUCAU Lucas <lucas.ducau64@gmail.com>
2023-05-30 09:48:40 +0200

    Index reorg

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
-<?php include './src/config/config.php'; 
+<?php 
+include './src/config/config.php'; 
 require_once(CLASSES . '/Router.php');
+
 $routes = include 'src/config/routes.php';
 $router = new Router();
 $page = $router->getPage();
 
 $titre = isset($routes[$page]) ? $routes[$page] : "404";
 $pageLink = CSS . "/" . $page . ".css";
+
+include TEMPLATE . "/base.php"; 
 ?>
-<?php include TEMPLATE . "/base.php"; ?>
