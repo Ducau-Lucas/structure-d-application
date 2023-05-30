f1b1c4cdd95ff881610b3e8025e9bfc1e0bd5bb2
DUCAU Lucas <lucas.ducau64@gmail.com>
2023-05-30 09:59:30 +0200

    Config reorg

--- src/config/config.php
+++ src/config/config.php
@@ -3,10 +3,11 @@
 define('TEMPLATE', './src/template');
 define('TEMPLATE_PARTS', './src/template/template_parts');
 define('PAGES', './src/template/pages/');
+define('FONCTIONS', './src/fonctions');
+define('CLASSES', './src/classes');
+
 define('IMAGES', './public/assets/img');
 define('CSS', './public/assets/css');
 define('JS', './public/assets/js');
-define('TITLE', 'Pierre, Paul, Jacques');
 
-define('FONCTIONS', './src/fonctions');
-define('CLASSES', './src/classes');
+define('TITLE', 'Pierre, Paul, Jacques');
