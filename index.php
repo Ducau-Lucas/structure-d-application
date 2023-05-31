9f6a56e945ec2e5942099c7c7149b267e7e15a22
DUCAU Lucas <lucas.ducau64@gmail.com>
2023-05-31 10:45:51 +0200

    Kernel bootstraping

--- index.php
+++ index.php
@@ -2,26 +2,7 @@
 include './src/config/config.php'; 
 require_once(CLASSES . '/Router.php');
 require_once(CLASSES . '/Controller.php');
-$routes = include 'src/config/routes.php';
-$router = new Router();
+require_once(CLASSES . '/Kernel.php');
 
-
-// START ------------------------------------------------
-
-$controller = new Controller($router);
-    // $controller->render($router);
-    // déterminer la méthode de controller à appeler en fonction de la page ($router->getPage())
-    $routes = include './src/config/routes.php';
-    if(array_key_exists($router->getPage(), $routes)){
-        $methodName = $routes[$router->getPage()];
-        var_dump($methodName);
-        $controller->$methodName();
-    }
-    var_dump($routes);
-
-    
-
-// STOP ----------------------------------------------------
-
-//include TEMPLATE . "/base.php"; 
-?>
+$kernel = new Kernel();
+$kernel->bootstrap();
---
+++ src/classes/Kernel.php
@@ -0,0 +1,21 @@
+<?php
+
+class Kernel {
+
+    
+    function bootstrap(){
+    
+        $routes = include 'src/config/routes.php';
+        $router = new Router();
+        $controller = new Controller($router);
+        // $controller->render($router);
+        // déterminer la méthode de controller à appeler en fonction de la page ($router->getPage())
+        $routes = include './src/config/routes.php';
+        if(array_key_exists($router->getPage(), $routes)){
+            $methodName = $routes[$router->getPage()];
+            var_dump($methodName);
+            $controller->$methodName();
+        }
+    
+    }
+}
