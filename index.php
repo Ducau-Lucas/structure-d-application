715ab43078754ba5cf5f43f5c9a477e7b24183aa
DUCAU Lucas <lucas.ducau64@gmail.com>
2023-05-31 10:02:47 +0200

    Controller finalization

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 include './src/config/config.php'; 
 require_once(CLASSES . '/Router.php');
-
+require_once(CLASSES . '/Controller.php');
 $routes = include 'src/config/routes.php';
 $router = new Router();
 
@@ -19,52 +19,7 @@ $controller = new Controller($router);
     }
     var_dump($routes);
 
-    class Controller
-    {
-        private $router;
-        
-        public function __construct($router)
-        {
-            $this->router = $router;
-        }
-
-        public function accueil(){
-            //Specific code for the home page
-            //TODO
-            $titre = "Accueil";
-            $this->render($titre);
-        }
-        
-        public function about(){
-            $titre = "A Propos";
-            $this->render($titre);
-        }
-
-        public function services(){
-            $titre = "Services";
-            $this->render($titre);
-        }
-        
-        public function contact(){
-            $titre = "Contact";
-            $this->render($titre);
-        }
-        
-        
-        public function render($titre=null){
-            
-            $pageLink = CSS . "/" . $this->router->getPage() . ".css";
-            if (file_exists(PAGES . $this->router->getPage() . ".php")) {
-                $template = PAGES . $this->router->getPage() . ".php";
-                //include PAGES . $this->router->getPage() . ".php";
-            } else {
-                $template = PAGES . "page404.php";
-                //include PAGES . "page404.php";
-            }
-            include TEMPLATE . "/base.php";
-            
-        }
-    }
+    
 
 // STOP ----------------------------------------------------
 
--- src/classes/Controller.php
+++ src/classes/Controller.php
@@ -1,2 +1,48 @@
 <?php
 
+class Controller
+    {
+        private $router;
+        
+        public function __construct($router)
+        {
+            $this->router = $router;
+        }
+
+        public function accueil(){
+            //Specific code for the home page
+            //TODO
+            $titre = "Accueil";
+            $this->render($titre);
+        }
+        
+        public function about(){
+            $titre = "A Propos";
+            $this->render($titre);
+        }
+
+        public function services(){
+            $titre = "Services";
+            $this->render($titre);
+        }
+        
+        public function contact(){
+            $titre = "Contact";
+            $this->render($titre);
+        }
+        
+        
+        public function render($titre=null){
+            
+            $pageLink = CSS . "/" . $this->router->getPage() . ".css";
+            if (file_exists(PAGES . $this->router->getPage() . ".php")) {
+                $template = PAGES . $this->router->getPage() . ".php";
+                //include PAGES . $this->router->getPage() . ".php";
+            } else {
+                $template = PAGES . "page404.php";
+                //include PAGES . "page404.php";
+            }
+            include TEMPLATE . "/base.php";
+            
+        }
+    }
