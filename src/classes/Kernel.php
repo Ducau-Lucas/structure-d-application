7aaec0f6ad60869f55cbeefcaeef6519a678cdbb
DUCAU Lucas <lucas.ducau64@gmail.com>
2023-05-31 11:07:59 +0200

    Final Commit

--- src/classes/Kernel.php
+++ src/classes/Kernel.php
@@ -5,18 +5,16 @@ class Kernel {
     
     function bootstrap(){
     
-        $routes = include 'src/config/routes.php';
         $router = new Router();
         $controller = new Controller($router);
-        // $controller->render($router);
         // déterminer la méthode de controller à appeler en fonction de la page ($router->getPage())
         $routes = include './src/config/routes.php';
         if(array_key_exists($router->getPage(), $routes)){
             $methodName = $routes[$router->getPage()];
-            $controller->$methodName();
         } else {
-            $controller->notFound();
+            $methodName = "notFound";
         }
+        $controller->$methodName();
     
     }
 }
